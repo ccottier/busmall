69a623d2abf3a97edf4880d708830084d38fed01
ccottier <chrystiecottier@gmail.com>
2017-02-25 15:01:11 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// global variables
+var productImages = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntan', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+var productsArray = [];
+
+function Products() {
+  
+}
+]
